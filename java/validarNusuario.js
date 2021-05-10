a263e35c05617f4a753556e9bd5f8588d3b1f10c
Zinnox41 <andres.cortezs@outlook.com>
2021-05-10 11:34:51 -0400

    validacion de registro
    a

--- java/validarNusuario.js
+++ java/validarNusuario.js
@@ -1,29 +1,31 @@
-$(document).ready(function () {
-    $("#error").hide(); //ocultarfeederror
-    $("#formulario_CrearUsuario").submit(function () {
-        var mensaje = "Revisa los siguientes detalles en el formulario";
-        if ($("#nombreu").val().trim().length == 0) {
-            mensaje = mensaje + " / " + " Usuario no debe estar vacio";
-        }
-        if ($("#correo").val().trim().length == 0) {
-            mensaje = mensaje + " / " + " Debe ingresar un correo valido";
-        }
-        if ($("#correo").val().trim().length == 0) {
-            mensaje = mensaje + " / " + " Debe ingresar un correo valido";
-        }
-        if ($("#contra").val().trim().length == 0) {
-            mensaje = mensaje + " / " + " Debe ingresar una contraseña ";
-        }
-        if ($(contra.value != contra2.value)) {
-            mensaje = mensaje + " / " + " Debe ingresar una contraseña ";
-        }
-        if ($("#contra2").val().trim().length == 0) {
-            mensaje = mensaje + " / " + " Contraseña no coincide ";
-        }
-        if (mensaje != "Revisa los siguientes detalles en el formulario") {
-            $("#error").html(mensaje);
-            $("#error").show();
-            event.preventDefault();
-        }
-    });
+$(document).ready(function(){
+  $("#error").hide(); //ocultarfeederror
+  $("#formulario_CrearUsuario").submit(function(){
+    var mensaje = "Revisa los siguientes detalles en el formulario";
+    //Usuario
+    if($("#nombreu").val().trim().length == 0){
+      mensaje = mensaje + "<br>USUARIO" + " <br>-No ingreso usuario";
+    }
+    
+    //Correo
+    if($("#correo").val().trim().length == 0){
+      mensaje = mensaje + "<br>email:" + "<br>No ingreso su correo electronico";
+    }
+    //Contraseña
+    if ($("#contra").val() != $("#ccontra").val()) {
+      mensaje = mensaje + "<br>CONTRASEÑA:" + "<br>-Las contraseñas deben ser iguales";
+    }
+    if ($("#contra").val().trim().length == 0) {
+      mensaje = mensaje + "<br>-La contraseña no debe estar vacia";
+    }
+   
+    //Terminos
+    if ($("#checko").is(':not(:checked)')) {
+    }
+    if(mensaje != "Revisa los siguientes detalles en el formulario"){
+      $("#error").html(mensaje);
+      $("#error").show();
+      event.preventDefault();
+    }
+  });
 });
