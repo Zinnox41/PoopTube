53398718295042c3132443b65eed882bd2bb3669
LoncoErick <erickmadridriquelme@gmail.com>
2021-06-05 23:49:18 -0400

    Arreglos y Htmls
    Agregue todos los html al proyecto y lo deje funcionando (masomenos)

--- tube/urls.py
+++ tube/urls.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import principal, historial
+from .views import principal, historial, favorito, pooppremium, contactanos, crear, ingresar, contra, reproductor
 
 
 urlpatterns = [
     path('',principal, name="principal"),
-    path('historial', historial, name="historial")
+    path('historial', historial, name="historial"),
+    path('favorito', favorito, name="favorito"),
+    path('pooppremium', pooppremium, name="pooppremium"),
+    path('contactanos',contactanos, name="contactanos"),
+    path('crear', crear, name="crear"),
+    path('ingresar', ingresar, name="ingresar"),
+    path('contra', contra, name="contra"),
+    path('reproductor', reproductor, name="reproductor")
 ]
     
--- tube/views.py
+++ tube/views.py
@@ -7,3 +7,25 @@ def principal(request):
 
 def historial(request):
     return render(request, 'tube/historial.html')
+
+def favorito(request):
+    return render(request, 'tube/favorito.html')
+
+
+def pooppremium(request):
+    return render(request, 'tube/pooppremium.html')
+
+def contactanos(request):
+    return render(request, 'tube/contactanos.html')
+
+def crear(request):
+    return render(request, 'tube/crear.html')
+
+def ingresar(request):
+    return render(request, 'tube/ingresar.html')
+
+def contra(request):
+    return render(request, 'tube/contra.html')
+
+def reproductor(request):
+    return render(request, 'tube/reproductor.html')
