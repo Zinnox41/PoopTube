befe24c4af300a37fe51b87894848e91109414a9
Alex-290 <81701606+Alex-290@users.noreply.github.com>
2021-06-08 20:49:34 -0400

    las tablas de admin

--- poopDjango/settings.py
+++ poopDjango/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.oracle',
         'NAME': '127.0.0.1:1521/xe',
-        'USER':'pooptube005 ',
+        'USER':'pooptube000 ',
         'PASSWORD':'12345',
         'TEST':{
             'USER':'default_test',
--- tube/admin.py
+++ tube/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Main, Favorito, Usuario, Reproductor
 # Register your models here.
+admin.site.register(Main)
+admin.site.register(Favorito)
+admin.site.register(Usuario)
+admin.site.register(Reproductor)
